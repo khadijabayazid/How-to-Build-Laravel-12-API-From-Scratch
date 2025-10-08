47a848d3a67feb3c75698089cfea0bba68161501
khadijabayazid <khokhaba2005@gmail.com>
2025-10-09 00:05:29 +0300

    Getting Single Record and API Resources

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
-        return Category::all();
+        // return Category::all();
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category){
+        // return $category;
+        return new CategoryResource($category);
+    }
+
+    public function list(){
+        return CategoryResource::collection(Category::all());
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,7 @@ class Category extends Model
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
     protected $fillable =[
-        'name'
+        'name',
+        'description',
     ];
 }
