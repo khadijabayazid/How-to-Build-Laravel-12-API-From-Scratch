ce3fc19a505606bae61555ba6dab229b68717223
khadijabayazid <khokhaba2005@gmail.com>
2025-10-15 23:13:09 +0300

    List inside of List: Multi-Level Data

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use League\CommonMark\Node\Block\Paragraph;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'description' => fake()->Paragraph(),
+            'price' => rand(1000, 99999),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Category::factory(10)->create();
+        Product::factory(20)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
