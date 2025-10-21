b807731f97dc046b76e17ec8bba8080490efb64f
khadijabayazid <khokhaba2005@gmail.com>
2025-10-21 11:46:00 +0300

    Finishing CRUD: Update, Delete and Resource Controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -27,5 +28,16 @@ class CategoryController extends Controller
     public function store(StoreCategoryRequest $request){
         $category = Category::create($request->all());
         return new CategoryResource($category);
+    }
+
+    public function update(Category $category, StoreCategoryRequest $request){
+        $category->update($request->all());
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category){
+        $category->delete();
+        // return response(null, Response::HTTP_NO_CONTENT);
+        return response()->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,11 @@ Route::get('/user', function (Request $request) {
 
 Route::get('lists/categories', [CategoryController::class, 'list']);
 
-Route::get('categories', [CategoryController::class ,'index']);
-Route::get('categories/{category}',[CategoryController::class, 'show']);
-Route::post('categories', [CategoryController::class, 'store']);
+// Route::get('categories', [CategoryController::class ,'index']);
+// Route::get('categories/{category}',[CategoryController::class, 'show']);
+// Route::post('categories', [CategoryController::class, 'store']);
+// Route::put('categories/{category}', [CategoryController::class, 'update']);
+// Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
 
+Route::resource('categories', CategoryController::class);
 Route::get('products', [ProductController::class , 'index']);
